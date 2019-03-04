72fd1bb426d2abe479cee1d27a7d4b2d4d882803
yjw1268 <yjw981213@163.com>
2019-03-04 16:36:49 +0800

    V2.0
    For linux with SQL

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import requests
 from requests.adapters import HTTPAdapter
@@ -87,14 +88,6 @@ class Pixiv(object):
                 sys.exit()
         return int(getnumber)
 
-    # def secontect(self, target_url):
-    #     s = se.get(self.target_url_1 + str(target_url))  # https://www.pixiv.net/setting_user.php
-    #     print("s")
-    #     print(s.text)
-    #     with open(self.load_path + 'Re.html', 'w', encoding='utf-8') as f:
-    #         f.write(s.text)
-    #         print("sc")
-
     def validateTitle(self, title):
         rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
         new_title = re.sub(rstr, "_", title)  # 替换为下划线
@@ -103,35 +96,9 @@ class Pixiv(object):
     def beautifulsoup(self, get_number, target_url):
         s = se.get(self.target_url_1 + str(target_url))
         soup = BeautifulSoup(s.text, features="html.parser")  # 初始化
-        # with open(self.load_path + 'Re_soup.html', 'w', encoding='utf-8') as f:
-        #     f.write(soup.prettify())  # 保存soup以便check
-        # 拉取原图
-        # title = soup.find_all("a", class_="title", limit=get_number)  # 寻找每周前get_number
-        # src_headers = self.headers
-        # for i in title:
-        #     temp_url = self.main_url + '/' + i['href']
-        #     # print(temp_url)  # 详细页的url
-        #     temp_clear = se.get(temp_url, headers=src_headers)
-        #     clear_soup = BeautifulSoup(temp_clear.text, features="html.parser")
-        #     name = self.validateTitle(i.string)  # 图片名称
-        #     op = clear_soup.prettify().find('"original":"')
-        #     ed = clear_soup.prettify().find('},"tags')
-        #     original_url = clear_soup.prettify()[op + 12:ed - 1]
-        #     adapt_url = original_url.replace('\/', '/')
-        #     # print(adapt_url)
-        #     # img = se.get(adapt_url, headers=src_headers)
-        #     # with open(self.load_path + name + '.jpg', 'wb') as f:  # 图片要用b,对text要合法化处理
-        #     #     f.write(img.content)  # 保存图片
-        #     # print("Finish")
-        #     time.sleep(1)
-        # 拉取缩略图
         # 数据库操作
-        db = pymysql.connect("localhost", "root", "PlanetarAntimony", "Study")
+        db = pymysql.connect("localhost", "root", "psd", "Study")
         cursor = db.cursor()
-        cursor.execute("SELECT VERSION()")
-        data = cursor.fetchone()
-        print("Database version : %s " % data)
-        # 关闭数据库连接
         # 图片处理
         pic_dl = soup.find_all("img", class_="_thumbnail ui-scroll-view", limit=get_number)
         title = soup.find_all("a", class_="title", limit=get_number)
@@ -144,29 +111,23 @@ class Pixiv(object):
             dlname = str(target_url) + '_' + time.strftime("%Y-%m-%d", time.localtime()) + '_' + str(i) + '.jpg'
             with open(self.load_path + dlname, 'wb') as f:  # 图片要用wb,对text要合法化处理
                 f.write(img.content)  # 保存图片
-                # 输出存储url
-                print(pic_dl_url)
-                # 获取原图url
-                temp_url = self.main_url + '/' + title[i]['href']
-                # print(temp_url)  # 详细页的url
-                temp_clear = se.get(temp_url, headers=self.headers)
-                clear_soup = BeautifulSoup(temp_clear.text, features="html.parser")
-                name = self.validateTitle(title[i].string)  # 图片名称
-                op = clear_soup.prettify().find('"original":"')
-                ed = clear_soup.prettify().find('},"tags')
-                original_url = clear_soup.prettify()[op + 12:ed - 1]
-                adapt_url = original_url.replace('\/', '/')
-                # print(adapt_url)
-                local_url = 'http://study.imoe.club/Try/rank_pic/' + dlname
-                print(local_url)
-                print("<br>")
-                sql = "INSERT INTO pixiv_rank(raw_url, original_url,local_url,title, love) VALUES ('%s', '%s','%s','%s', '%s')" % \
-                      (str(pic_dl_url),adapt_url,str(local_url),name, 0)
-                # 执行sql语句
-                cursor.execute(sql)
-                # 执行sql语句
-                db.commit()
-                print("1")
+            # 获取原图url
+            temp_url = self.main_url + '/' + title[i]['href']
+            temp_clear = se.get(temp_url, headers=self.headers)
+            clear_soup = BeautifulSoup(temp_clear.text, features="html.parser")
+            name = self.validateTitle(title[i].text)  # 图片名称
+            op = clear_soup.prettify().find('"original":"')
+            ed = clear_soup.prettify().find('},"tags')
+            original_url = clear_soup.prettify()[op + 12:ed - 1]
+            adapt_url = original_url.replace('\/', '/')
+            local_url = 'http://study.imoe.club/Try/rank_pic/' + dlname
+            # print(local_url)
+            # print("<br>")
+            sql = "INSERT INTO pixiv_rank(raw_url, pixiv_id,original_url,local_url,title, love) VALUES ('%s', '%s','%s','%s','%s', '%s')" % \
+                  (str(pic_dl_url), pic_id, str(adapt_url), str(local_url), name, 0)
+            # 执行sql语句
+            cursor.execute(sql)
+            db.commit()
             i += 1
         db.close()
 
@@ -174,7 +135,6 @@ class Pixiv(object):
 if __name__ == '__main__':
     pixiv = Pixiv()
     pixiv.login()
-    # print("Welcome!")
     mode = pixiv.choosemode()
     pixiv.beautifulsoup(pixiv.getnum(mode), mode)
     print("System Exit")
